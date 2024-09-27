b53789f7285c3b2f5f6812a6ce5ed2172a015b40
asdfghjA1 <aaryan14decembersingh@gmail.com>
2024-09-28 03:56:43 +0530

    Unverified Commit

--- 1-token-launchpad-starter/src/App.jsx
+++ 1-token-launchpad-starter/src/App.jsx
@@ -1,10 +1,33 @@
-import './App.css'
-import { TokenLaunchpad } from './components/TokenLaunchpad'
+import {
+  ConnectionProvider,
+  WalletProvider,
+} from "@solana/wallet-adapter-react";
+
+import {
+  WalletModalProvider,
+  WalletDisconnectButton,
+  WalletMultiButton,
+} from "@solana/wallet-adapter-react-ui";
+import "@solana/wallet-adapter-react-ui/styles.css";
+import "./App.css";
+import { TokenLaunchpad } from "./components/TokenLaunchpad";
+import { UserBalance } from "./components/userBalance";
 
 function App() {
   return (
-    <TokenLaunchpad></TokenLaunchpad>
-  )
+    <ConnectionProvider endpoint={"https://api.devnet.solana.com"}>
+      <WalletProvider wallets={[]} autoConnect>
+        <WalletModalProvider>
+          <WalletMultiButton />
+
+          <UserBalance />
+          <TokenLaunchpad>
+            <WalletDisconnectButton />
+          </TokenLaunchpad>
+        </WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
+  );
 }
 
-export default App
+export default App;
--- 1-token-launchpad-starter/src/components/TokenLaunchpad.jsx
+++ 1-token-launchpad-starter/src/components/TokenLaunchpad.jsx
@@ -1,17 +1,92 @@
+import { getMinimumBalanceForRentExemptAccount } from "@solana/spl-token";
+import { MINT_SIZE } from "@solana/spl-token/lib/types";
+import { useConnection, useWallet } from "@solana/wallet-adapter-react";
+import { Keypair, SystemProgram, Transaction } from "@solana/web3.js";
 
 export function TokenLaunchpad() {
-    return  <div style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        flexDirection: 'column'
-    }}>
-        <h1>Solana Token Launchpad</h1>
-        <input className='inputText' type='text' placeholder='Name'></input> <br />
-        <input className='inputText' type='text' placeholder='Symbol'></input> <br />
-        <input className='inputText' type='text' placeholder='Image URL'></input> <br />
-        <input className='inputText' type='text' placeholder='Initial Supply'></input> <br />
-        <button className='btn'>Create a token</button>
+  const waller = useWallet();
+  const {connection } = useConnection();
+  async function createToken() {
+    const name = document.getElementById("name").value;
+    const symbol = document.getElementById("symbol").value;
+    const imageUrl = document.getElementById("imageUrl").value;
+    const intialSupply = document.getElementById("intialSupply").value;
+
+    const lamports = await getMinimumBalanceForRentExemptAccount(connection);
+    const keypair = Keypair.generate();
+
+    const transaction = new Transaction().add(
+        SystemProgram.createAccount({
+            fromPubkey : payer.publicKey,
+            newAccountPubkey : keypair.publicKey,
+            space : MINT_SIZE , 
+            lamports,
+            programId
+        })
+    )
+
+}
+  return (
+    <div
+      style={{
+        height: "100vh",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column",
+      }}
+    >
+      <h1
+        style={{
+          color: "#00ffcc",
+          textShadow: "0 0 10px rgba(0, 255, 204, 0.5)",
+        }}
+      >
+        Solana Token Launchpad
+      </h1>
+      <input
+        id="name"
+        className="inputText"
+        type="text"
+        placeholder="Name"
+        style={{ border: "1px solid #00ffcc", borderRadius: "5px" }}
+      ></input>{" "}
+      <br />
+      <input
+        id="symbol"
+        className="inputText"
+        type="text"
+        placeholder="Symbol"
+        style={{ border: "1px solid #00ffcc", borderRadius: "5px" }}
+      ></input>{" "}
+      <br />
+      <input
+        id="imageUrl"
+        className="inputText"
+        type="text"
+        placeholder="Image URL"
+        style={{ border: "1px solid #00ffcc", borderRadius: "5px" }}
+      ></input>{" "}
+      <br />
+      <input
+        id="intialSupply"
+        className="inputText"
+        type="text"
+        placeholder="Initial Supply"
+        style={{ border: "1px solid #00ffcc", borderRadius: "5px" }}
+      ></input>{" "}
+      <br />
+      <button
+        onClick={createToken}
+        className="btn"
+        style={{
+          backgroundColor: "#00ffcc",
+          color: "#000",
+          transition: "background-color 0.3s",
+        }}
+      >
+        Create a token
+      </button>
     </div>
+  );
 }
